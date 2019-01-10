4b2adc46f2516edfe1531d556c9af5de4710e347
RuslanZinchenko <zinchenko.rusya@gmail.com>
2019-01-10 11:34:13 +0200

    addBtn

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -5,6 +5,53 @@ var findQuery = document.querySelector('.form__input');
 var findBtn = document.querySelector('.form__button');
 var gallery = document.querySelector('.gallery__wrapper');
 var btnHidd = document.querySelector('.btn-hidden');
+btnHidd.addEventListener('click', appendGallery);
+
+function loadImages(image) {
+  var apiUrl = "https://api.pexels.com/v1/search?query=".concat(image, "&per_page=18");
+  var headers = {
+    Authorization: apiKey
+  };
+  return fetch(apiUrl, {
+    headers: headers
+  }).then(function (response) {
+    if (response.ok) return response.json();
+    throw new Error('error' + response.statusText);
+  }).then(function (data) {
+    return data.photos;
+  }).catch(function (err) {
+    return console.log(err);
+  });
+}
+
+;
+
+function appendGallery(event) {
+  event.preventDefault();
+
+  var updatePage = function updatePage(photos) {
+    var append = imageMaker(photos);
+    gallery.innerHTML += append;
+  };
+
+  loadImages(findQuery.value).then(updatePage);
+}
+
+;
+
+function imageMaker(img) {
+  var markup = img.reduce(function (markup, item) {
+    return markup + "<div class=\"img-list__imgs\"> <img src=".concat(item.src.large, " \n    alt=").concat(item.photographer, " class=\"imgs__item\"> </div>");
+  }, '');
+  return markup;
+}
+'use strict';
+
+var apiKey = '563492ad6f91700001000001efe4554ec5924beab8b1c433b156ab30';
+var findQuery = document.querySelector('.form__input');
+var findBtn = document.querySelector('.form__button');
+var gallery = document.querySelector('.gallery__wrapper');
+var btnHidd = document.querySelector('.btn-hidden');
 findBtn.addEventListener('click', goApi);
 
 function goApi(e) {
